c39e3d5ece773dba67fb935e17d64857033304bf
yangzhe <306503157@qq.com>
2020-12-07 21:27:38 +0800

    添加主函数，以及utils

---
+++ model/main.py
@@ -0,0 +1,102 @@
+from model.utils import padding_mask, generate_batch_index, generate_batch_AdjMatrix
+import pickle
+from layer.SRGNN import SRGNNLayer
+import os
+import tensorflow as tf
+from collections import defaultdict
+import csv
+
+# 确保可以复现
+tf.set_random_seed(0)
+
+# diginetica 43105
+class Main(object):
+    def __init__(self):
+        self.train_data, self.train_label, self.test_data, self.test_label = self.get_inputs(
+            path='../processed_data/diginetica_sample')
+        self.train_mask, self.train_sess, self.train_label = padding_mask(self.train_data, self.train_label)
+        self.test_mask, self.test_sess, self.test_label = padding_mask(self.test_data, self.test_label)
+        self.metrics = defaultdict(list)
+        self.srgnn = SRGNNLayer(num_item=312, hidden_size=100, learning_rate=1e-3,
+                                l2_weight=1e-5, ggnn_step=1, topk=20, log=True)
+        self.train_writer = tf.summary.FileWriter(logdir='../tensorboard/train', graph=tf.get_default_graph())
+        self.test_writer = tf.summary.FileWriter(logdir='../tensorboard/test')
+        self.train(epoch=10, batch_size=100)
+        self.train_writer.close()
+        self.test_writer.close()
+        self.save_metrics(path='../metrics/sample1.csv')
+
+    def get_inputs(self, path):
+        with open(os.path.join(path, 'train.pkl'), 'rb') as f:
+            train_data, train_label = pickle.load(f)
+        with open(os.path.join(path, 'test.pkl'), 'rb') as f:
+            test_data, test_label = pickle.load(f)
+        return train_data, train_label, test_data, test_label
+
+    def get_feed_dict(self, batch_index, sess, label, mask):
+        batch_sess = sess[batch_index]
+        batch_label = label[batch_index]
+        batch_mask = mask[batch_index]
+        batch_out_adj, batch_in_adj, batch_padding_sess, batch_input_index = generate_batch_AdjMatrix(
+            batch_sess)
+        feed_dict = {self.srgnn.item_sess: batch_padding_sess, self.srgnn.label: batch_label,
+                     self.srgnn.mask: batch_mask, self.srgnn.input_index: batch_input_index,
+                     self.srgnn.in_adj: batch_in_adj, self.srgnn.out_adj: batch_out_adj}
+        return feed_dict
+
+    def train(self, epoch, batch_size):
+        total_samples = len(self.train_sess)
+        for i in range(epoch):
+            loss_epoch, precision_epoch, mrr_epoch = 0, 0, 0
+            batch_index_list = generate_batch_index(total_samples, batch_size=batch_size, seed=i)
+            for batch_index in batch_index_list:
+                feed_dict = self.get_feed_dict(batch_index, self.train_sess, self.train_label, self.train_mask)
+                fetches = [self.srgnn.merge_ops, self.srgnn.global_step,
+                           self.srgnn.loss, self.srgnn.batch_pre, self.srgnn.batch_mrr, self.srgnn.optimizer]
+                merged_summary, step, batch_loss, batch_precision, batch_mrr, _ = self.srgnn.sess.run(fetches, feed_dict)
+                self.train_writer.add_summary(merged_summary, step)
+                loss_epoch += batch_loss * len(batch_index)
+                precision_epoch += batch_precision * len(batch_index)
+                mrr_epoch += batch_mrr * len(batch_index)
+            self.metrics['train_loss'].append(loss_epoch / total_samples)
+            self.metrics['train_precision'].append(precision_epoch / total_samples)
+            self.metrics['train_mrr'].append(mrr_epoch / total_samples)
+            print('================epoch : {}================'.format(i))
+            print('train_loss : {}, P@20 : {}, MRR@20 : {}'.format(self.metrics['train_loss'][-1],
+                                                                   self.metrics['train_precision'][-1],
+                                                                   self.metrics['train_mrr'][-1]))
+            self.test(batch_size)
+
+    # batch_size应尽可能大
+    def test(self, batch_size):
+        loss_epoch, precision_epoch, mrr_epoch = 0, 0, 0
+        total_samples = len(self.test_sess)
+        batch_index_list = generate_batch_index(total_samples, batch_size=batch_size, seed=0)
+        for i, batch_index in enumerate(batch_index_list):
+            feed_dict = self.get_feed_dict(batch_index, self.test_sess, self.test_label, self.test_mask)
+            fetches = [self.srgnn.merge_ops, self.srgnn.global_step,
+                       self.srgnn.loss, self.srgnn.batch_pre, self.srgnn.batch_mrr]
+            merged_summary, step, batch_loss, batch_precision, batch_mrr = self.srgnn.sess.run(fetches, feed_dict)
+            loss_epoch += batch_loss * len(batch_index)
+            precision_epoch += batch_precision * len(batch_index)
+            mrr_epoch += batch_mrr * len(batch_index)
+        # 只使用最后一个batch作为可视化
+        self.test_writer.add_summary(merged_summary, step)
+        self.metrics['test_loss'].append(loss_epoch / total_samples)
+        self.metrics['test_precision'].append(precision_epoch / total_samples)
+        self.metrics['test_mrr'].append(mrr_epoch / total_samples)
+        print('test_loss : {}, P@20 : {}, MRR@20 : {}'.format(self.metrics['test_loss'][-1],
+                                                               self.metrics['test_precision'][-1],
+                                                               self.metrics['test_mrr'][-1]))
+
+    def save_metrics(self, path):
+        with open(path, 'w', newline='') as f:
+            writer = csv.writer(f)
+            head = ['epoch'] + list(self.metrics.keys())
+            writer.writerow(head)
+            for epoch in range(len(self.metrics['train_loss'])):
+                row = [epoch] + [self.metrics[key][epoch] for key in self.metrics.keys()]
+                writer.writerow(row)
+
+if __name__ == '__main__':
+    main = Main()
---
+++ model/utils.py
@@ -0,0 +1,60 @@
+import numpy as np
+from itertools import chain
+
+def padding_mask(data, label):
+    maxlen = max([len(sess) for sess in data])
+    mask = [[1] * len(sess) + [0] * (maxlen - len(sess)) for sess in data]
+    padding_sess = [sess + [0] * (maxlen - len(sess)) for sess in data]
+    return np.array(mask), np.array(padding_sess), np.array(label)
+
+def get_unique_item_nums(data):
+    if isinstance(data, np.ndarray):
+        return np.unique(data).shape[0]
+    elif isinstance(data, list):
+        flatten_data = chain.from_iterable(data)
+        return len(set(flatten_data))
+    else:
+        raise TypeError('传参类型错误，必须为np.array或者list(list)类型参数')
+
+def generate_batch_index(total_samples, batch_size, seed):
+    np.random.seed(seed)
+    index = np.random.permutation(np.arange(total_samples))
+    num_batches = (total_samples - 1) // batch_size + 1
+    batch_index_list = []
+    for i in range(num_batches):
+        batch_index_list.append(index[i * batch_size: min((i + 1) * batch_size, total_samples)])
+    return batch_index_list
+
+def encoder(input):
+    vocab = {}
+    for item in input:
+        if item not in vocab:
+            vocab[item] = len(vocab)
+    return vocab
+
+def generate_batch_AdjMatrix(input):
+    if not isinstance(input, np.ndarray) or input.ndim != 2:
+        raise TypeError('输入应该为padding后的二维数组')
+    batch_in_adj_matrix, batch_out_adj_matrix, batch_unique_item, batch_input_index = [], [], [], []
+    # 批量化处理，要求每个batch内的每个session的邻接矩阵具有相同的维度
+    max_node = max([len(np.unique(sess)) for sess in input])
+    for sess in input:
+        item_vocab = encoder(sess)
+        padding_item = list(item_vocab.keys()) + [0] * (max_node - len(item_vocab))
+        batch_unique_item.append(padding_item)
+        adj_matrix = np.zeros((max_node, max_node))
+        for idx in range(len(sess) - 1):
+            if sess[idx + 1] == 0:
+                break
+            u = item_vocab[sess[idx]]
+            v = item_vocab[sess[idx + 1]]
+            adj_matrix[u, v] = 1
+        out_matrix = adj_matrix
+        in_matrix = adj_matrix.transpose()
+        out_degree = np.clip(np.sum(out_matrix, axis=1, keepdims=True), 1, None)
+        in_degree = np.clip(np.sum(in_matrix, axis=1, keepdims=True), 1, None)
+        batch_out_adj_matrix.append(np.divide(in_matrix, in_degree))
+        batch_in_adj_matrix.append(np.divide(out_matrix, out_degree))
+        batch_input_index.append([item_vocab[item] for item in sess])
+    return np.array(batch_out_adj_matrix), np.array(batch_in_adj_matrix), np.array(batch_unique_item), np.array(
+        batch_input_index)
